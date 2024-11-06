0bd0bc4438ad3a9a791abcaeeffec6d210ad561a
kawtharnasser <kawtherhabib.ko@gmail.com>
2024-11-06 12:18:10 +0300

    completed ex 8 and 9

--- app.js
+++ app.js
@@ -91,3 +91,33 @@ Complete Exercise 8 in the space below:
 */
 let soyIdx = foods.indexOf('tofu')
 console.log('Exercise 8 result:', soyIdx)
+
+/*
+Exercise 9: Joining elements
+
+1) Use the `join()` method to concatenate the strings in the `foods` array, 
+   separated by ' -> '. 
+
+2) Assign the result to a variable called `allFoods`. 
+
+Note: The final result should log as:
+'taco -> sushi -> cupcake -> tofu -> cheeseburger'
+
+Complete Exercise 9 in the space below:
+*/
+let allFoods = foods.join(' -> ')
+
+console.log('Exercise 9 result:', allFoods)
+
+/*
+Exercise 10: Check for an element
+
+1) Using the .includes() method, check if the `foods` array contains the string
+   'soup'.
+
+2) Assign the result to a variable called `hasSoup``.
+
+Complete Exercise 10 in the space below:
+*/
+let hasSoup = foods.includes('soup')
+console.log('Exercise 10 result:', hasSoup)
